c379dd5f0938eb5f8b45469e99af35a8481ee794
zhangjun <zhangjun@qiatuchina.com>
2018-09-04 14:45:59 +0800

    init

--- src/timer/timerTest1/Run1.java
+++ src/timer/timerTest1/Run1.java
@@ -1,5 +1,7 @@
 package timer.timerTest1;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 import java.util.Timer;
@@ -7,8 +9,6 @@ import java.util.TimerTask;
 
 public class Run1 {
     public static Timer timer = new Timer();
-
-
     public static class MyTask extends TimerTask {
 
         @Override
@@ -17,8 +17,10 @@ public class Run1 {
         }
     }
 
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws ParseException {
+        MyTask task = new MyTask();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date dateRef = sdf.parse("2018-08-31 08:49:00");
+        System.out.println("字符串时间："+dateRef.toLocaleString());
     }
 }
